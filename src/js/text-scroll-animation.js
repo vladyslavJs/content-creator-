352c37869b5bb072e0fc6d3cbb11002adc14284b
vladyslavJs <vladyslavmelnyk15@gmail.com>
2025-04-09 22:52:04 +0200

    Update text-scroll-animation.js

--- src/js/text-scroll-animation.js
+++ src/js/text-scroll-animation.js
@@ -4,17 +4,18 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 export function textScrollAnimation() {
-    gsap.to(".about-text", {
-        scrollTrigger: {
-            trigger: ".aboutme-section",
-            start: "top center",
-            end: "bottom top",
-            scrub: true,
-        },
-        y: -100,
-        opacity: 0,
-        scale: 0.9,
-    });
+    gsap.to(".about-text",
+        {
+            scrollTrigger: {
+                trigger: ".aboutme-section",
+                start: "top center",
+                end: "bottom top",
+                scrub: true,
+            },
+            y: -100,
+            opacity: 0,
+            scale: 1,
+        });
     gsap.to(".portfolio-section", {
         scrollTrigger: {
             trigger: ".aboutme-section",
