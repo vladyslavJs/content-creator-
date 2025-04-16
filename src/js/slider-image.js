05bd457634160d6ae4ffa5310bae7f5e4cd50289
vladyslavJs <vladyslavmelnyk15@gmail.com>
2025-04-16 17:59:05 +0200

    Create slider-image.js

---
+++ src/js/slider-image.js
@@ -0,0 +1,28 @@
+const images = [
+    "src/assets/images/hero-image.JPG",
+    "src/assets/images/hero-section.JPG",
+    "src/assets/images/photographer.jpeg"
+];
+
+let currentIndex = 0;
+
+export function showNextImage() {
+    const sliderImage = document.getElementById("slider-image");
+
+    // Перевіряємо, чи елемент існує
+    if (!sliderImage) {
+        console.log("slider-image element not found");
+        return;
+    }
+
+    setInterval(() => {
+        currentIndex = (currentIndex + 1) % images.length;
+
+        sliderImage.style.opacity = 0;
+
+        setTimeout(() => {
+            sliderImage.src = images[currentIndex];
+            sliderImage.style.opacity = 1;
+        }, 400);
+    }, 4000);
+}
