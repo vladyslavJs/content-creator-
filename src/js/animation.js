fa2499fa8c97ac127feb0cca721953b62283823b
vladyslavJs <vladyslavmelnyk15@gmail.com>
2025-05-05 16:37:07 +0200

    Update animation.js

--- src/js/animation.js
+++ src/js/animation.js
@@ -48,36 +48,3 @@ export function arrowAnimation() {
         ease: "elastic"
     });
 }
-
-// export function textAnimation() {
-//     gsap.from(".portfolio-first-text", {
-//         scrollTrigger: {
-//             trigger: ".portfolio-first-text",
-//             start: "top 80%",
-//             toggleActions: "play none none play reset",
-//             markers: false
-//         },
-//         opacity: 0,
-//         duration: 1,
-//         y: 30,
-//         ease: "power2.out"
-//     })
-// }
-
-export function textAnimation() {
-    gsap.from(".portfolio-first-text, .portfolio-second-text, .portfolio-third-text", {
-        scrollTrigger: {
-            trigger: ".portfolio-first-text",
-            start: "top 90%",
-            end: "bottom top",
-            toggleActions: "play play play reset"
-            // markers: false,
-            // scrub: true
-        },
-        opacity: 0,
-        y: 30,
-        duration: 0.5,
-        delay: 0.5,
-        ease: "power1.out"
-    });
-}
